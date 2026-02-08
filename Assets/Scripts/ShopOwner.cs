d4a4a8daaf41a9e33c003c5a9ad031e56f971e9a
Turhan0 <turhan.rafizade@gmail.com>
2026-02-08 23:41:34 +0100

    Added shop owner and interaction with her

---
+++ Assets/Scripts/ShopOwner.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class ShopOwner : MonoBehaviour
+{
+    public static bool shopUImenuActivated = false;
+    public GameObject shopUIMenu;
+    public bool playerInRange;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        if (!shopUImenuActivated && playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 0f;
+            shopUIMenu.SetActive(true);
+            shopUImenuActivated = true;
+        }
+        else if (shopUImenuActivated && playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 1f;
+            shopUIMenu.SetActive(false);
+            shopUImenuActivated = false;
+        }
+    }
+
+
+}
