a8d51b7a65ec63dc6cae8aa72333620552a5f0ae
Turhan0 <turhan.rafizade@gmail.com>
2026-02-14 17:13:49 +0100

    Added audio feedback to several menus and buttons

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,6 +7,9 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource SFXsource;
     [SerializeField] private AudioClip purchaseSound;
     [SerializeField] private AudioClip purchaseFailSound;
+    [SerializeField] private AudioClip discardSound;
+    [SerializeField] private AudioClip menuOpenSound;
+    [SerializeField] private AudioClip menuCloseSound;
 
 
     void Awake()
@@ -29,6 +32,18 @@ public class AudioManager : MonoBehaviour
     public void PlayPurchaseFailSound()
     {
         SFXsource.PlayOneShot(purchaseFailSound);
+    }
+    public void PlayDiscardSound()
+    {
+        SFXsource.PlayOneShot(discardSound);
+    }
+    public void PlayMenuOpenSound()
+    {
+        SFXsource.PlayOneShot(menuOpenSound);
+    }
+        public void PlayMenuCloseSound()
+    {
+        SFXsource.PlayOneShot(menuCloseSound);
     }
 
 }
